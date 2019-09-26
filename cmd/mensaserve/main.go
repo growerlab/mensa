f1285f18853f4188433f5fc1b90dad0676526b4c
Force Charlie <fcharlie@users.noreply.github.com>
2019-09-26 08:29:26 +0800

    rename

--- cmd/mensaserve/main.go
+++ cmd/mensaserve/main.go
@@ -1,5 +1,7 @@
 package main
 
+// HTTP feature
+
 func main() {
 
 }
---
+++ cmd/mensasshd/main.go
@@ -0,0 +1,12 @@
+package main
+
+// mensasshd
+
+// "github.com/growerlab/mensa/mensa"
+// var (
+// 	me = mensa.AuthContext{}
+// )
+
+func main() {
+	//fmt.Fprintf(os.Stderr, "%v", me)
+}
