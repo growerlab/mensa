2b9fca1cf21f5b695c76684e0a480c22bf39465b
moli <molisoft@qq.com>
2019-12-26 16:05:20 +0800

    fix: log

--- cmd/main.go
+++ cmd/main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	fmt.Println("=================================")
-	defer fmt.Println("=================================")
 	fmt.Println(mensa.UA)
 	fmt.Println("BuiltTime: ", mensa.BUILDTIME)
 	fmt.Println("CommitID: ", mensa.BUILDCOMMIT)
 	fmt.Println("GoVersion: ", mensa.GOVERSION)
+	fmt.Println("=================================")
 
 	if err := mensa.Run(); err != nil {
 		panic(err)
